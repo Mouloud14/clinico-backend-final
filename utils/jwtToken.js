26ee81610c687619e0023c0bbf91c9dac886ba1b
Mouloud <mouloudka18392@gmail.com>
2025-06-13 19:33:26 +0100

    FIX: Cookie options for production (secure, sameSite)

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -32,9 +32,6 @@ export const cookieOptions = { // <<< AJOUTEZ CET OBJET D'OPTIONS
   sameSite: 'none',
 };
 
-export const generateToken = (user, message, statusCode, res) => {
-  const token = user.generateJsonWebToken();
-  const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
 
   res
     .status(statusCode)
