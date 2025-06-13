d8ee549db7c85691649952d28b88eb58fbd7be3f
Mouloud <mouloudka18392@gmail.com>
2025-06-13 19:48:36 +0100

    CORS FIX: Authorize Vercel main project URL for origin (final-final attempt)

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -20,24 +20,3 @@ export const generateToken = (user, message, statusCode, res) => {
       token,
     });
 };
-
-// ... (votre code existant) ...
-
-export const cookieOptions = { // <<< AJOUTEZ CET OBJET D'OPTIONS
-  expires: new Date(
-    Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-  ),
-  httpOnly: true,
-  secure: true,
-  sameSite: 'none',
-};
-
-  res
-    .status(statusCode)
-    .cookie(cookieName, token, cookieOptions) // Utilisez l'objet cookieOptions ici
-    .json({
-      success: true,
-      message,
-      user,
-      token,
-    });
