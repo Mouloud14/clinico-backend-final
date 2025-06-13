a32d3f7d6d407dd7bfd1343960e1013c9d37c629
Mouloud <mouloudka18392@gmail.com>
2025-06-13 18:53:19 +0100

    CORS FIX: Authorize exact Vercel project URL including scope

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ console.log("CORS Origin configured as:", process.env.DASHBOARD_URL);
 
 app.use(
   cors({
-    origin: "https://clinico-dashboard.vercel.app",
+    origin: "https://clinico-dashboard-moulouds-projects-0f7926a8.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
